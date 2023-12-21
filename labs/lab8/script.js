244b722ee28135ed98e5760331427de978e97dcc
irisli1 <144866080+irisli1@users.noreply.github.com>
2023-12-21 13:42:34 -0800

    Delete labs directory

--- labs/lab8/script.js
+++
@@ -1,7 +0,0 @@
-function toggleTheme() {
-    var peaBody = document.querySelector("body");
-    peaBody.classList.toggle("dark-mode");
-}
-
-let toggleButton = document.getElementById("toggleButton")
-toggleButton.onclick = toggleTheme;
